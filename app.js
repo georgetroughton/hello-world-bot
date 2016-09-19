d4ac0ab28801c33eb87cd704f3bc5d89d320af1e
George <george@Georges-MacBook-Pro.local>
2016-09-19 14:43:46 +0100

    Created package.json and app.js

--- app.js
+++ app.js
@@ -5,5 +5,15 @@ let restify = require('restify'),
 //set up restify server
 let server = restify.createServer();
 server.listen(process.env.PORT || 3000, () => {
-	
+	console.log('%s listening to $s', server.name, server.url);
+});
+
+//create chat bot
+let connector = new builder.ChatConnector({appId: 'id', appPassword: 'password'});
+let bot = new buider.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//create bot dialogs
+bot.dialog('/', (session) => {
+	session.send("Hello World!");
 });
